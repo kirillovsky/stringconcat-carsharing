71392f6ffa82c821ae02f1913e5cccb4e1a9941f
AlexKirillov <Kirillov.A.Valer@sberbank.ru>
2021-07-20 22:28:33 +0300

    remove useless event

--- customer/domain/src/main/kotlin/com/stringconcat/kirillov/carsharing/customer/CustomerEvents.kt
+++ customer/domain/src/main/kotlin/com/stringconcat/kirillov/carsharing/customer/CustomerEvents.kt
@@ -4,4 +4,3 @@ import com.stringconcat.kirillov.carsharing.commons.types.base.DomainEvent
 
 data class CustomerRegistered(val value: CustomerId) : DomainEvent()
 data class CustomerRejected(val value: CustomerId) : DomainEvent()
-data class CustomerVerified(val value: CustomerId) : DomainEvent()
